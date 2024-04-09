ee154a36134e4ab9196ac283bcc9fb6eaba68cc9
pauljako <paulifey18@gmail.com>
2024-04-09 18:44:31 +0200

    Added --launch option

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+import argparse
+
 import interface
 import notification
+import session
 
 if __name__ == "__main__":
-    notification.send("EGL Running", "Easy Game Launcher is Running", 1500)
-    interface.main()
+    parser = argparse.ArgumentParser(prog="Easy Game Launcher")
+
+    parser.add_argument("--launch", "-l")
+
+    args = parser.parse_args()
+
+    if args.launch is None:
+        notification.send("EGL Running", "Easy Game Launcher is Running", 1500)
+        interface.main()
+    else:
+        notification.send("Game started", f"Game {args.launch} started.", 1500)
+        session.new_session(args.launch)
