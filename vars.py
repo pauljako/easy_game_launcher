b2bebf2e7c0158e6e049eb0c57ce69146f99b6b8
pauljako <paulifey18@gmail.com>
2024-04-09 18:43:39 +0200

    Added SESSION_PATH

--- vars.py
+++ vars.py
@@ -7,3 +7,5 @@ ICON_DIR = os.path.realpath(os.path.join(CONFIG_DIR, "icons"))
 GAME_PATH = os.path.realpath(os.path.join(CONFIG_DIR, "games.json"))
 
 ACCOUNT_PATH = os.path.realpath(os.path.join(CONFIG_DIR, "account.json"))
+
+SESSION_PATH = os.path.realpath(os.path.join(CONFIG_DIR, "session.pkl"))
