9eaae8338ce576a27d03bd2e02f49b982ecb4e34
pauljako <paulifey18@gmail.com>
2024-04-09 18:43:51 +0200

    Now using Classes

--- session.py
+++ session.py
@@ -1,4 +1,7 @@
 import json
+# import pickle
+import dill as pickle
+
 import notification
 import os
 import vars
@@ -8,57 +11,76 @@ import threading
 with open(vars.GAME_PATH, "rb") as f:
     games = json.load(f)
 
-status = "Online"
 
-game_thread: threading.Thread
+class Session:
+    def __init__(self, name: str, games_list: dict, status: str):
+        self.name = name
+        self.games = games_list
+        self.game_thread = threading.Thread(name=name, target=self.run)
+        self.status = status
 
-session = ""
+    def run(self):
+        cmd = self.games[self.name]["cmd"]
+        before_time = time.time()
+        os.system(cmd)
+        took = time.time() - before_time
+        if took >= 30.0:
+            print("Exiting Session")
+            exit_session()
+        else:
+            print("Command exited too Fast")
+
+    def start(self):
+        if not self.game_thread.is_alive():
+            self.game_thread.start()
+
+    def set_status(self, new_status: str = "Online"):
+        self.status = new_status
 
 
-def set_status(new_status: str = "Online"):
-    global status
-    status = new_status
-
-
-def get_session() -> str | None:
-    global session
-    if session == "":
+def get_session() -> Session | None:
+    if not os.path.exists(vars.SESSION_PATH):
         return None
+    try:
+        with open(vars.SESSION_PATH, "rb") as f:
+            return pickle.load(f)
+    except EOFError:
+        return None
+
+
+def get_status() -> str:
+    session = get_session()
+    if session is None:
+        return "Online"
     else:
-        return session
+        return session.status
+
+
+def set_status(new_status: str = "Online") -> None:
+    session = get_session()
+    if session is None:
+        return
+    session.status = new_status
+    with open(vars.SESSION_PATH, "wb") as f:
+        pickle.dump(session, f)
 
 
 def exit_session():
-    global game_thread, session
     if get_session() is None:
         notification.send("Cannot exit session", "There is no Game running", 4000)
         return
-    session = ""
-    set_status()
-
-
-def run(name: str):
-    global games
-    cmd = games[name]["cmd"]
-    before_time = time.time()
-    os.system(cmd)
-    took = time.time() - before_time
-    if took >= 30.0:
-        print("Exiting Session")
-        exit_session()
-    else:
-        print("Command exited too Fast")
+    os.remove(vars.SESSION_PATH)
 
 
 def new_session(name: str):
-    global games, game_thread, session
+    global games
     if get_session() is not None:
         notification.send(f"Could not start {name}.", "Another Game is already Running", 4000)
         return
     if name not in games or "cmd" not in games[name]:
         notification.send(f"Could not start {name}.", "Game not found", 4000)
         return
-    session = name
-    game_thread = threading.Thread(name=name, args=(name,), target=run)
-    game_thread.start()
-    set_status("Playing " + name)
+    session = Session(name, games, "Playing " + name)
+    with open(vars.SESSION_PATH, "wb") as f:
+        pickle.dump(session, f)
+    get_session().start()
