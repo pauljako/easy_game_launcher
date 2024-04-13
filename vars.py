8cc0607b78315572127a2d1be9e903246e463053
pauljako <paulifey18@gmail.com>
2024-04-13 09:36:20 +0200

    Update

--- vars.py
+++ vars.py
@@ -1,5 +1,7 @@
 import os
 
+VERBOSE = True
+
 CONFIG_DIR = os.path.realpath(os.path.join(os.path.expanduser("~"), "EGL"))
 
 ICON_DIR = os.path.realpath(os.path.join(CONFIG_DIR, "icons"))
@@ -9,3 +11,7 @@ GAME_PATH = os.path.realpath(os.path.join(CONFIG_DIR, "games.json"))
 ACCOUNT_PATH = os.path.realpath(os.path.join(CONFIG_DIR, "account.json"))
 
 SESSION_PATH = os.path.realpath(os.path.join(CONFIG_DIR, "session.pkl"))
+
+TICK_OBJ_PATH = os.path.realpath(os.path.join(CONFIG_DIR, "tick.pkl"))
+
+TICK_THREAD_PATH = os.path.realpath(os.path.join(CONFIG_DIR, "tick_thread.pkl"))
