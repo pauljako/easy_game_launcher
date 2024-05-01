501b1ec088067a4bdcf9f560fb6b1096591daa57
pauljako <paulifey18@gmail.com>
2024-05-01 11:54:22 +0200

    Bug Fix

--- messages.py
+++ messages.py
@@ -14,18 +14,33 @@ def process_messages():
     if os.path.exists(vars.IS_RUNNING_MSG_PATH):
         if vars.VERBOSE:
             print("[ Message | Info ] Processing Hook: is-running")
-        os.remove(vars.IS_RUNNING_MSG_PATH)
+        while True:
+            try:
+                os.remove(vars.IS_RUNNING_MSG_PATH)
+                break
+            except:
+                print("Error")
     if os.path.exists(vars.KILL_MSG_PATH):
         if vars.VERBOSE:
             print("[ Message | Info ] Processing Hook: kill")
-        os.remove(vars.KILL_MSG_PATH)
+        while True:
+            try:
+                os.remove(vars.KILL_MSG_PATH)
+                break
+            except:
+                print("Error")
         if session.get_session() is not None:
             session.exit_session()
         sys.exit()
     if os.path.exists(vars.OPEN_UI_MSG_PATH):
         if vars.VERBOSE:
             print("[ Message | Info ] Processing Hook: open-ui")
-        os.remove(vars.OPEN_UI_MSG_PATH)
+        while True:
+            try:
+                os.remove(vars.OPEN_UI_MSG_PATH)
+                break
+            except:
+                print("Error")
         if interface.app is not None:
             if vars.VERBOSE:
                 print("[ Message | Warning ] Another Window is already opened")
@@ -36,11 +51,21 @@ def process_messages():
             print("[ Message | Info ] Processing Hook: launch")
         with open(vars.LAUNCH_GAME_MSG_PATH, "rt") as f:
             game = f.read()
-        os.remove(vars.LAUNCH_GAME_MSG_PATH)
+        while True:
+            try:
+                os.remove(vars.LAUNCH_GAME_MSG_PATH)
+                break
+            except:
+                print("Error")
         notification.send("Game started", f"Game {game} started.", 1500)
         session.new_session(game)
     if os.path.exists(vars.EXIT_MSG_PATH):
         if vars.VERBOSE:
             print("[ Message | Info ] Processing Hook: exit")
-        os.remove(vars.EXIT_MSG_PATH)
+        while True:
+            try:
+                os.remove(vars.EXIT_MSG_PATH)
+                break
+            except:
+                print("Error")
         session.exit_session()
