7da8c571d3fc01ec3fd7dcae9e475555647b94a1
pauljako <paulifey18@proton.me>
2024-10-15 19:20:56 +0200

    Update

---
+++ boundaries_run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+pushd "$APP_DIR" &> /dev/null
+
+if [ ! -d "venv" ]; then
+  echo "[ Pre Launcher | Warning ] Could not find venv. Using system installation"
+  launch_command="python3 main.py $@"
+else
+  launch_command="venv/bin/python3 main.py $@"
+fi
+
+$launch_command
+
+popd &> /dev/null
